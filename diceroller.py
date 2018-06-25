1a06b0cf34af4f9c66b8616405fe16a50c52cd02
Tilia <lindsey.snider@gmail.com>
2018-06-24 21:05:02 -0700

    Added an info msgs and error msg

--- diceroller.py
+++ diceroller.py
@@ -2,7 +2,8 @@
 from random import randint
 
 #dice = input("Dice to roll: ")
-
+info_msg = "\nWelcome to Tilia's Dice Roller.\n\nType a number followed by the letter \"d\" followed by another number to roll some dice.\nType \"q\" to exit.\nType \"help\" to repeat this prompt. \n"
+error_msg = "\nThat's not a valid dice roll. Please try again.\n"
 def sanitize(string):
     string = string.lower()
     newstring = ""
@@ -13,13 +14,21 @@ def sanitize(string):
 
 def diceroller():
 
-    cont = True
-    while cont== True:
+    
+    while True:
         dicestring = sanitize(input("Dice to roll: "))
 
         num = ""
         die = ""
         print(dicestring)
+        if dicestring == "q":
+            break
+        elif dicestring == "help":
+            print(info_msg)
+            continue
+        elif len(dicestring) < 3 or "d" not in dicestring:
+            print(error_msg)
+            continue
         for i in range(len(dicestring)):
 
             if dicestring[i] == "d":
@@ -45,5 +54,5 @@ def diceroller():
             cont = False
             print("Thanks for rolling")
 
-
+print(info_msg)
 diceroller()
